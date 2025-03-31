4c8a391c051e25731aa3c80de3a0db32a82bad80
cokaluk <cokaluk@academic.rrc.ca>
2025-03-31 12:24:08 -0500

    generated user model using devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -12,7 +13,7 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-   root "products#index"
+   root to: "products#index"
 
    resources :products, only: [:index, :show]
    resources :categories, only: [:index, :show]

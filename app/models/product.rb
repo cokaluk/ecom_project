263b5b0d13e43a01722209563151dd3b6c7be394
cokaluk <cokaluk@academic.rrc.ca>
2025-04-07 08:32:39 -0500

    changed sign-up and edit user pages to require all data be supplied, added validations to rails models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,16 @@
 class Product < ApplicationRecord
   belongs_to :category
 
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :stock, presence: true
+  validates :description, presence: true
 
   def self.ransackable_attributes(auth_object = nil)
-    ["category_id", "created_at", "description", "id", "id_value", "name", "price", "stock", "updated_at"]
+    [ "category_id", "created_at", "description", "id", "id_value", "name", "price", "stock", "updated_at" ]
   end
 
   def self.ransackable_associations(auth_object = nil)
-    ["category"]
+    [ "category" ]
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :province_id, presence: true
+
+  validates :name, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
 end

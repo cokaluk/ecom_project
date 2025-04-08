ab16d1e69dc856c67624d0f700e2010ca883f153
cokaluk <cokaluk@academic.rrc.ca>
2025-04-08 15:37:27 -0500

    added ability to add multiple of one product to cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,21 +2,34 @@ class CartsController < ApplicationController
   before_action :initialize_cart
 
   def show
-    @products = Product.find(@cart)
+    @cart = session[:cart]
   end
 
   def add_item
-    product_id = params[:product_id].to_i
-    @cart << product_id unless @cart.include?(product_id)
-    session[:cart] = @cart
-    redirect_to cart_path, notice: "Product added to cart."
+    logger.debug "Cart before adding: #{session[:cart].inspect}"
+    product = Product.find(params[:product_id])
+    quantity = params[:quantity].to_i
+
+    logger.debug "Adding product: #{product.name}, Quantity: #{quantity}"
+
+    existing_product = session[:cart].find { |item| item["id"] == product.id }
+
+    if existing_product
+      logger.debug "Product found in cart, updating quantity. current quantity: #{existing_product[:quantity]}"
+      existing_product["quantity"] += quantity
+    else
+      logger.debug "Product not found in cart, adding new product."
+      session[:cart] ||= []
+      session[:cart] << {"id" => product.id, "name" => product.name, "price" => product.price, "quantity" => quantity }
+    end
+
+    redirect_to cart_path
   end
 
   def remove_item
     product_id = params[:product_id].to_i
-    @cart.delete(product_id)
-    session[:cart] = @cart
-    redirect_to cart_path, notice: "Product removed from cart."
+    session[:cart].reject! { |product| product["id"] == product_id}
+    redirect_to cart_path
   end
 
   private

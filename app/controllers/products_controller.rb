3d157860b067bf36f947e44162ddd1da33f85139
Cody Okaluk <cokaluk@academic.rrc.ca>
2025-04-15 12:07:19 -0500

    added search form to products index page as well as categories index and show pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,17 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all.page(params[:page]).per(10)
+    @categories = Category.all
+    @products = Product.all
+
+    if params[:category_id].present?
+      @products = @products.where(category_id: params[:category_id])
+    end
+
+    if params[:query].present?
+      @products = @products.where("name LIKE :q OR description LIKE :q", q: "%#{params[:query]}%")
+    end
+
+    @products = @products.page(params[:page]).per(10)
   end
 
   def show
